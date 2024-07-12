ec276af37144ddeca000da97975753ae7fb68b64
Yu Kai Him Otto <112672032+ottoykh@users.noreply.github.com>
2024-07-12 11:02:05 +0800

    fix the segment_address error.

--- NLP/zhSeg.py
+++ NLP/zhSeg.py
@@ -25,13 +25,6 @@ def segment_text_file(file_path):
     df = pd.DataFrame(segmented_lines)
     return df
 
-
-building_names = set()
-with open("/content/Geocoding/NLP/Building_merged.txt", 'r', encoding='utf-8') as f:
-    for line in f:
-        building_names.add(line.strip())
-
-
 def segment_address(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         text = file.read()
@@ -39,7 +32,7 @@ def segment_address(file_path):
     lines = text.splitlines()
     segmented_data = []
     road_related_words = ['路', '道', '街', '巷', '橋', '隧道', '大道', '高速公路', '公路', '馬路', '徑']
-
+    
     areas = {
         '香港': {
             '中西區': ['堅尼地城', '石塘咀', '西營盤', '上環', '中環', '金鐘', '半山', '山頂'],
@@ -72,30 +65,24 @@ def segment_address(file_path):
         for district, sub_districts in districts.items():
             all_districts.append(district)
             all_districts.extend(sub_districts)
-
+    
     for line in lines:
-        words = jieba.lcut(line)
+        words = pseg.cut(line)
         street_name = []
         street_number = []
         district = ''
         sub_district = ''
         area = ''
-        building = ''
         number_found = False
-
-        for i, word in enumerate(words):
-            if '號' in word and not number_found:
-                street_number.append(word.split('號')[0] + '號')
-                number_found = True
-                if i + 1 < len(words):
-                    next_word = words[i + 1]
-
-                    building_keywords = ['居', '大廈', '園', '城', '樓', '苑', '閣', '別墅', '中心','匯']
-
-                    if any(keyword in next_word for keyword in building_keywords):
-                        building = next_word
-            elif any(road_word in word for road_word in road_related_words):
+        
+        for word, flag in words:
+            if any(road_word in word for road_word in road_related_words):
                 street_name.append(word)
+            elif (flag == 'm' or '號' in word) and not number_found:
+                street_number.append(word)
+                if '號' in word:
+                    number_found = True
+                    street_number[-1] = street_number[-1].split('號')[0] + '號'
             elif word in all_districts:
                 for area_name, districts in areas.items():
                     for district_name, sub_districts in districts.items():
@@ -110,16 +97,16 @@ def segment_address(file_path):
                             break
                     if area:
                         break
-
-        if street_name or street_number or district or sub_district or building:
+        
+        if street_name or street_number or district or sub_district:
             segmented_data.append({
                 'original': line,
                 'area': area,
                 'district': district,
                 'sub_district': sub_district,
                 'street_name': ' '.join(street_name),
-                'street_number': ' '.join(street_number),
-                'building': building
+                'street_number': ' '.join(street_number)
             })
 
-    df = pd.DataFrame(
+    df = pd.DataFrame(segmented_data)
+    return df
